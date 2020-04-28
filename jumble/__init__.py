387c7dfba58f6743197136c233a6a7840e754511
Virginio Sannibale <vsanni@localhost.localdomain>
2020-04-27 21:22:48 -0700

    jumble/__init__.py: added standard variables

---
+++ jumble/__init__.py
@@ -0,0 +1,7 @@
+__author__     = "vsanni"
+__email__      = "vsanni@local.com"
+
+__name__       = "jumble"
+__version__    = "" #@Version@ please, do not edit this line
+__status__     = "" #@Status@ please, do not edit this line
+__date__       = "02/10/2019" #@Date@ please, do not edit this line
