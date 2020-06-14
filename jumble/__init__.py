02525c65d8736a6319dc7475c2bd13820f3f74ef
Virginio Sannibale <sannibalev@gmail.com>
2020-06-13 18:35:56 -0700

    tag version commit: 1.0.alpha

--- jumble/__init__.py
+++ jumble/__init__.py
@@ -2,6 +2,6 @@ __author__     = "vsanni"
 __email__      = "vsanni@local.com"
 
 __name__       = "jumble"
-__version__    = "" #@Version@ please, do not edit this line
-__status__     = "" #@Status@ please, do not edit this line
-__date__       = "02/10/2019" #@Date@ please, do not edit this line
+__version__    = "1.0" #@Version@ please, do not edit this line
+__status__     = "alpha" #@Status@ please, do not edit this line
+__date__       = "06/13/2020" #@Date@ please, do not edit this line
